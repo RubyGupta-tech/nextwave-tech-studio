09a8c4ff034bd1e1b546bf04d0577c3c75ab273b
RubyGupta-tech <webserviesbygupta@gmail.com>
2026-04-13 09:32:24 -0700

    UI: Transformed Resources Hub with cinematic high-definition background and premium overlays

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,13 +6,9 @@ import "../src/styles/global.css";
 const Hero = () => {
     return (
         <section className="hero">
-            {/* Background Light Energy */}
-            <div className="light-pool" style={{ top: '-10%', left: '-5%' }}></div>
-            <div className="light-pool light-pool-magenta" style={{ bottom: '-10%', right: '-5%' }}></div>
-
             <div className="hero-waves-container">
                 <svg className="hero-wave wave-1" viewBox="0 0 1440 320" preserveAspectRatio="none">
-                    <path fill="rgba(6, 182, 212, 0.1)" d="M0,96L48,112C96,128,192,160,288,186.7C384,213,480,235,576,213.3C672,192,768,128,864,128C960,128,1056,192,1152,208C1248,224,1344,192,1392,176L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+                    <path fill="rgba(26, 188, 156, 0.05)" d="M0,96L48,112C96,128,192,160,288,186.7C384,213,480,235,576,213.3C672,192,768,128,864,128C960,128,1056,192,1152,208C1248,224,1344,192,1392,176L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
                 </svg>
                 <div className="hero-glow-trails">
                     {[...Array(40)].map((_, i) => (
